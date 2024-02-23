12905db73d764ea47715f5129eef7196a3f8db67
Badreddine Yacine YACHEUR <fb_yacheur@esi.dz>
2024-02-23 20:47:39 +0100

    Delete for clean code

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/byacheur/Apps/artery/scenarios/hybrid_comm
-# Build directory: /home/byacheur/Apps/artery/scenarios/hybrid_comm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
